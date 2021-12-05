4c62a322113a2fd4469ef797a3c1471488db1467
alex <strim0125@yandex.ru>
2021-12-05 20:07:31 +0300

    1.3

--- src/ru/geekbrains/common/part2_lesson4/Main.java
+++ src/ru/geekbrains/common/part2_lesson4/Main.java
@@ -5,10 +5,10 @@ public class Main {
     public static void main(String[] args) {
         Employe[] employees = new Employe[5];
         employees[0] = new Employe("Ivanov Ivan", "developer", "ivanov@emplyees.ru", 2421917, 23000, 21);
-        employees[1] = new Employe("Petrov Andrey", "stroitel", "ivanov@emplyees.ru", 2421917, 23000, 31);
-        employees[2] = new Employe("Sidorov Vasiliy", "developer", "ivanov@emplyees.ru", 2421917, 23000, 26);
-        employees[3] = new Employe("Petechkin Petr", "developer", "ivanov@emplyees.ru", 2421917, 23000, 36);
-        employees[4] = new Employe("Vladykin Ivan", "developer", "ivanov@emplyees.ru", 2421917, 23000, 49);
+        employees[1] = new Employe("Petrov Andrey", "stroitel", "petrov@emplyees.ru", 2421920, 26000, 31);
+        employees[2] = new Employe("Sidorov Vasiliy", "dev", "sidirov@emplyees.ru", 2422315, 39000, 26);
+        employees[3] = new Employe("Petechkin Petr", "admin", "peteckin@emplyees.ru", 2421145, 35000, 36);
+        employees[4] = new Employe("Vladykin Ivan", "dir", "vladykin@emplyees.ru", 242461, 50000, 49);
 
         for (int i = 0; i < employees.length; i++) {
             if (employees[i].getAge() > 40)
